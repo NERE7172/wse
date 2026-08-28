31866423a96d474d6aba70c9e8cbb47c70051aff
NERE7172 <memoraengine@gmail.com>
2026-08-28 14:33:13 +0700

    Export token estimator from public API

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@ export {
   type OptimizerOptions,
   type OptimizationResult,
 } from "./optimizer";
+
+export {
+  estimateTokens,
+  estimateContextTokens,
+  type TokenEstimate,
+  type TokenEstimatorOptions,
+} from "./token-estimator";
