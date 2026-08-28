8cb5ee1f4f9eae5048dec67d13a2c3bd9295acaf
NERE7172 <memoraengine@gmail.com>
2026-08-28 14:44:06 +0700

    Export state delta API

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,10 @@ export {
   type TokenEstimate,
   type TokenEstimatorOptions,
 } from "./token-estimator";
+
+export {
+  computeStateDelta,
+  type StateRecord,
+  type StateChange,
+  type StateDelta,
+} from "./state-delta";
